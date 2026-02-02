c90b4a0e37591f2e5fea5528658506e2932d89a1
Vaturia <kkorzun19@gmail.com>
2026-02-02 14:24:50 +0300

    feat: add class Logger

---
+++ include/logger.hpp
@@ -0,0 +1,14 @@
+#include <spdlog/spdlog.h>
+
+class Logger
+{
+public:
+    Logger()
+    {
+        spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
+    }
+    void info(const std::string &message) {spdlog::info(message);}
+    void warn(const std::string &message) {spdlog::warn(message);}
+    void error(const std::string &message) {spdlog::error(message);}
+    void debug(const std::string &message) {spdlog::debug(message);}
+};
