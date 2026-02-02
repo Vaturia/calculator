1a2c93e70c69fbfda5a36a9eb37ab9e6200809ee
Vaturia <kkorzun19@gmail.com>
2026-02-02 14:25:48 +0300

    feat: refactor class Operation to struct

--- include/operation.hpp
+++ include/operation.hpp
@@ -1,38 +1,19 @@
 #pragma once
-
-class Parser;
-class Compute;
-class Printer;
-class App;
-
-class Operation
+struct Operation
 {
-public:
     enum class Mode{
         CALC,
         HELP,
         VERSION,
         ERROR
     };
-private:
+
     long m_first, m_second;
     char m_operator;
     long long m_result;
     Mode m_operationMode;
-
-public:
     Operation()
         :m_first{0}, m_second{1}, m_operator{0}, m_result{0}, m_operationMode{Mode::ERROR}
     {
     }
-    long getFirstNum() const { return m_first; };
-    long getSecondNum() const { return m_second; };
-    char getOperator() const { return m_operator; };
-    long long getResult() const { return m_result; };
-    Mode getMode() const { return m_operationMode; };
-    void setFirstNum(long num) { m_first = num; };
-    void setSecondNum(long num) { m_second = num; };
-    void setOperator(char op) { m_operator = op; };
-    void setResult (long long res) { m_result = res; };
-    void setMode (Mode operMode) { m_operationMode = operMode; };
 };
