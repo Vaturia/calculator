e991eb83c4b1a67878c19fed131dad8afef05f45
Vaturia <kkorzun19@gmail.com>
2026-02-18 20:24:19 +0300

    feat: change implementention(raw pointers to smart pointers)

--- include/db_conn.hpp
+++ include/db_conn.hpp
@@ -8,46 +8,54 @@
 #include "operation.hpp"
 #include <stdexcept>
 
+struct PGconnDeleter {
+    void operator()(PGconn* conn) const {
+        if(conn) PQfinish(conn);
+    }
+};
+struct PGresultDeleter {
+    void operator()(PGresult* res) const {
+        if(res) PQclear(res);
+    }
+};
+using PGconnPtr   = std::unique_ptr<PGconn,   PGconnDeleter>;
+using PGresultPtr = std::unique_ptr<PGresult, PGresultDeleter>;
 class DBConnection
 {
 private:
-    PGconn* m_conn = nullptr;
+    PGconnPtr m_conn;
 
 public:
     explicit DBConnection(const std::string &conn_info)
         : m_conn(PQconnectdb(conn_info.c_str()))
     {
-        if(PQstatus(m_conn) != CONNECTION_OK)
+        if(PQstatus(m_conn.get()) != CONNECTION_OK)
         {
-            std::string err = PQerrorMessage(m_conn);
-            PQfinish(m_conn);
+            std::string err = PQerrorMessage(m_conn.get());
             throw std::runtime_error("Connection failed: " + err);
         }
     }
 
     void exec(const std::string &sql)
     {
-        PGresult* res = PQexec(m_conn, sql.c_str());
-        if(PQresultStatus(res) != PGRES_COMMAND_OK)
+        PGresultPtr res(PQexec(m_conn.get(), sql.c_str()));
+        if(PQresultStatus(res.get()) != PGRES_COMMAND_OK)
         {
-            std::string err = PQerrorMessage(m_conn);
-            PQclear(res);
+            std::string err = PQerrorMessage(m_conn.get());
             throw std::runtime_error("Exec failed: " + err);
         }
-        PQclear(res);
     }
     std::vector<std::vector<std::string>> select(const std::string &sql)
     {
-        PGresult* res = PQexec(m_conn, sql.c_str());
-        if(PQresultStatus(res) != PGRES_TUPLES_OK)
+        PGresultPtr res(PQexec(m_conn.get(), sql.c_str()));
+        if(PQresultStatus(res.get()) != PGRES_TUPLES_OK)
         {
-            std::string err = PQerrorMessage(m_conn);
-            PQclear(res);
+            std::string err = PQerrorMessage(m_conn.get());
             throw std::runtime_error("Select failed: " + err);
         }
 
-        size_t rows = static_cast<size_t>(PQntuples(res));
-        size_t cols = static_cast<size_t>(PQnfields(res));
+        size_t rows = static_cast<size_t>(PQntuples(res.get()));
+        size_t cols = static_cast<size_t>(PQnfields(res.get()));
         std::vector<std::vector<std::string>> result;
         result.reserve(rows);      
         
@@ -57,21 +65,11 @@ public:
             row.reserve(cols);
             for(size_t j = 0; j < cols; ++j)
             {
-                row.emplace_back(PQgetvalue(res, i, j));
+                row.emplace_back(PQgetvalue(res.get(), i, j));
             }
             result.push_back(std::move(row));
         }
-
-        PQclear(res);
         return result;
     }
 
-    ~DBConnection()
-    {
-        if(m_conn)
-        {
-            PQfinish(m_conn);
-            m_conn = nullptr;
-        }
-    }
 };
